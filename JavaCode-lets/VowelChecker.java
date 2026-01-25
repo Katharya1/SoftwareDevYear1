dbe43e442ee4b5a4ad20cdad8d2977ff665248cd
Katharya1 <113615538+Katharya1@users.noreply.github.com>
2026-01-25 00:56:09 +0000

    Create VowelChecker.java

---
+++ JavaCode-lets/VowelChecker.java
@@ -0,0 +1,74 @@
+package com.mycompany.semester2.y; 
+import java.util.Scanner; 
+
+/** 
+* Purpose: Write a java program that takes a character input 
+* and determines whether it is a vowel or a consonant using 
+* Character Class 
+* @author Katharya
+* Student Number:  
+* Date: 14/01/2026 
+*/ 
+  
+public class VowelChecker { 
+    public static void main(String[] args) {   
+
+        //initialise Variables 
+        Scanner userInput = new Scanner(System.in); 
+        String inputString; 
+        String ch; 
+        int stringSize = 0; 
+        double testNumeric; 
+        
+        //User Query and set up for checks 
+        System.out.printf("Please enter a single alphabetic Character: "); 
+            inputString = userInput.nextLine(); 
+            stringSize = inputString.length(); 
+            ch = inputString.toLowerCase(); 
+         
+        //If Number - program closses. Otherwise continue. 
+        try { 
+            testNumeric = Double.parseDouble(inputString); 
+            if (testNumeric >= 0 || testNumeric <0)  
+            { 
+            System.out.println("Input is not an alphabetic character. Program Closing."); 
+                System.exit(0); 
+            } 
+        } catch (NumberFormatException e)  
+        {   
+        // intentially left empty     
+        } 
+
+        //Quick Size Check 
+        var stringCheck = (stringSize !=1 )? 1 : 2 ; 
+      
+        //Main Switch 
+        switch (stringCheck)  
+        { 
+            case 1: 
+                System.out.println("Input is not a single alphabetic character. Program Closing."); 
+                System.exit(0); 
+             break; 
+            
+            // Secondary Switch  
+            case 2: 
+                switch (ch) 
+                { 
+                    case "a","o","e","i","u": 
+                        System.out.println("Character '"+inputString+"' is a vowel"); 
+                        break; 
+  
+                    default:  
+                        System.out.println("Character '"+inputString+"' is not a vowel"); 
+                        break; 
+                } 
+             break; 
+        } 
+
+         
+
+    } 
+
+  
+
+} 
